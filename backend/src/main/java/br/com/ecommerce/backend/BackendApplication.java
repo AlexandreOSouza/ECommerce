90c37911b7f582a235003e46f5b6bedbbb29d76a
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-04-01 20:59:37 -0300

    Produto e associacao muitos para muitos

--- backend/src/main/java/br/com/ecommerce/backend/BackendApplication.java
+++ backend/src/main/java/br/com/ecommerce/backend/BackendApplication.java
@@ -1,7 +1,9 @@
 package br.com.ecommerce.backend;
 
 import br.com.ecommerce.backend.domain.Categoria;
+import br.com.ecommerce.backend.domain.Produto;
 import br.com.ecommerce.backend.repositoreis.CategoriaRepository;
+import br.com.ecommerce.backend.repositoreis.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,8 @@ public class BackendApplication implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
-
+    @Autowired
+    private ProdutoRepository produtoRepository;
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
@@ -25,7 +28,19 @@ public class BackendApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "Informatica");
         Categoria cat2 = new Categoria(null, "Escritorio");
 
+        Produto p1 = new Produto(null, "Computador", 2000.0);
+        Produto p2 = new Produto(null, "Impressora", 800.0);
+        Produto p3 = new Produto(null, "Mouse", 80.0);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2 ,p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
     }
 }
