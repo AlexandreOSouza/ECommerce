7a0e15514e20ea44e661aa8eaf5a63320c74cb0e
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-04-01 19:58:27 -0300

    Criando operacao de instanciacao

--- backend/src/main/java/br/com/ecommerce/backend/BackendApplication.java
+++ backend/src/main/java/br/com/ecommerce/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package br.com.ecommerce.backend;
 
+import br.com.ecommerce.backend.domain.Categoria;
+import br.com.ecommerce.backend.repositoreis.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 }
