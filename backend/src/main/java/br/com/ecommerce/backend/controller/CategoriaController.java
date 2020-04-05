49190d3cc3094828323a6922fd799c400ff048f7
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-04-05 19:14:22 -0300

    Inserindo uma nova Categoria com POST

--- backend/src/main/java/br/com/ecommerce/backend/controller/CategoriaController.java
+++ backend/src/main/java/br/com/ecommerce/backend/controller/CategoriaController.java
@@ -4,10 +4,10 @@ import br.com.ecommerce.backend.domain.Categoria;
 import br.com.ecommerce.backend.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -15,11 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaController {
 
     @Autowired
-    private CategoriaService service;
+    private CategoriaService categoriaService;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = service.buscar(id);
+        Categoria obj = categoriaService.buscar(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+        categoria = categoriaService.insert(categoria);
+        // Pega a URI do recurso -> ex: http://localhost:8080/categorias
+        // e insere o id da nova categoria, utilizando as boas praticas
+        // de um servico REST
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(categoria.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
